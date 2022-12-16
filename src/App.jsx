735ac028562fd5b4e52ffa16e7912d3b37f55bf6
Rohith <rohith4518@gmail.com>
2022-12-16 14:04:20 +0530

    added the routes / and /analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Provider } from "react-redux";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import AnalyticsBoard from "./components/AnalyticsBoard/AnalyticsBoard";
 import store from "./Redux/store";
@@ -7,12 +8,20 @@ function App() {
   return (
     <Provider store={store}>
       <div className="App">
-        <img
-          className="greedy-logo"
-          src="/greedyLogo.png"
-          alt="Greedy Game Logo"
-        />
-        <AnalyticsBoard></AnalyticsBoard>
+        <a href="https://greedygame.com/">
+          <img
+            className="greedy-logo"
+            src="/greedyLogo.png"
+            alt="Greedy Game Logo"
+          />
+        </a>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<AnalyticsBoard />}>
+              <Route index path="analytics" element={<AnalyticsBoard />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
       </div>
     </Provider>
   );
